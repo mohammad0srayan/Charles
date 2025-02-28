efa375acb482d50c2394aa88ccfe148814c2473e
mohammad <taskforcemmd@gmail.com>
2025-02-28 11:55:29 +0330

    Add Sidebar To Website

---
+++ assets/main.js
@@ -0,0 +1,10 @@
+function hidesidebar() {
+    const sidebar = document.querySelector(".sidebar");
+    sidebar.style.display = 'none'
+}
+
+function showsidebar() {
+    const sidebar = document.querySelector(".sidebar");
+    sidebar.style.display = 'flex'
+}
+
